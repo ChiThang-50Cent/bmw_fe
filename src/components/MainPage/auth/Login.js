d47f973c9241f6ab5a2d4e8b518b99454fe6a309
ChiThang-50Cent <chithang.nydo@gmail.com>
2022-05-17 02:30:24 +0700

    test

--- src/components/MainPage/auth/Login.js
+++ src/components/MainPage/auth/Login.js
@@ -5,13 +5,14 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 // import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 import axios from "axios"
 import Swal from 'sweetalert2';
 
@@ -104,14 +105,14 @@ export default function Login() {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
+                <Link to="#" variant="body2">
                   <h4>
                   Forgot password?
                   </h4>
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="/register" variant="body2">
+                <Link to="/register" variant="body2">
                   <h4>
                   {"Don't have an account? Sign Up"}
                   </h4>
