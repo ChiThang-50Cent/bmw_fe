d01551fd03dbedcd31b2ed40de54584ea8fffa8f
ChiThang-50Cent <chithang.nydo@gmail.com>
2022-05-17 14:16:33 +0700

    b

--- src/components/MainPage/auth/Login.js
+++ src/components/MainPage/auth/Login.js
@@ -35,8 +35,8 @@ export default function Login() {
         "https://26ee6717535d4fcdbf3413520ba1f619-b30b63d5-vm-80.vlab2.uit.edu.vn/api/user/login",
         { ...user }
       );
-      localStorage.setItem("Login", true);
       document.cookie = `refreshtoken=${res.refreshtoken}`;
+      localStorage.setItem("Login", true);
       window.location.href = "/";
     } catch (err) {
       Swal.fire({
