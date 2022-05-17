ecf6a1a73b0d0383822128f37662bdeaa7ead7c7
ChiThang-50Cent <chithang.nydo@gmail.com>
2022-05-17 14:59:28 +0700

    b

--- src/API/const.js
+++ src/API/const.js
@@ -1,2 +1,2 @@
 export const url =
-    "https://26ee6717535d4fcdbf3413520ba1f619-b30b63d5-vm-80.vlab2.uit.edu.vn/api";
+    "https://26ee6717535d4fcdbf3413520ba1f619-b30b63d5-vm-80.vlab2.uit.edu.vn";
